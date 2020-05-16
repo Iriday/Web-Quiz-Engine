af204069db220e99782ddab813991634841b7c17
Igor <GI_Something@protonmail.com>
2020-05-16 20:24:17 +0300

    feat: Add possibility to add/get/solve quizzes
    - Add -> "/api/quizzes" - {"title":"..","text":"..","options":["..",".."..],"answer":..(index in options)}.
    returns - {"id":..,"title":"..","text":"..","options":["..",".."..]} (the same but with assigned "id" and without "answer")
    - Get -> "/api/quizzes/{id}".
    returns - {"id":..,"title":"..","text":"..","options":["..",".."..]}
    - Solve -> "/api/quizzes/{id}/solve?answer=(index in options)"
    returns - {"success":true/false,"feedback":".."}

--- src/main/java/engine/Quiz.java
+++ src/main/java/engine/Quiz.java
@@ -1,14 +1,25 @@
 package engine;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Quiz {
+    private static int idCounter = 0;
+    private final int id = ++idCounter;
     private final String title;
     private final String text;
     private final String[] options;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private final int answer;
 
-    public Quiz(String title, String text, String[] options) {
+    public Quiz(String title, String text, String[] options, int answer) {
         this.title = title;
         this.text = text;
         this.options = options;
+        this.answer = answer;
+    }
+
+    public int getId() {
+        return id;
     }
 
     public String getTitle() {
@@ -21,5 +32,9 @@ public class Quiz {
 
     public String[] getOptions() {
         return options;
+    }
+
+    public int getAnswer() {
+        return answer;
     }
 }
--- src/main/java/engine/QuizController.java
+++ src/main/java/engine/QuizController.java
@@ -1,29 +1,44 @@
 package engine;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class QuizController {
-    private final Quiz testQuiz;
+    private final List<Quiz> quizzes = new ArrayList<>();
 
     public QuizController() {
-        testQuiz = new Quiz("The java Logo", "What is depicted on the Java logo?", new String[]{"Robot", "Tea leaf", "Cup of coffee", "bug"});
     }
 
-    @GetMapping(path = "/api/quiz")
-    public Quiz getQuiz() {
-        return testQuiz;
+    @PostMapping(path = "/api/quizzes")
+    public Quiz addQuiz(@RequestBody Quiz quiz) {
+        quizzes.add(quiz);
+        return quiz;
     }
 
-    @PostMapping(path = "/api/quiz")
-    public SolveQuizResponse solveQuiz(@RequestParam("answer") int answer) {
-        if (answer == 2) {
-            return new SolveQuizResponse(true, "Congratulations, you're right!");
-        } else {
-            return new SolveQuizResponse(false, "Wrong answer! Please, try again.");
+    @GetMapping(path = "/api/quizzes/{id}")
+    public ResponseEntity<Quiz> getQuiz(@PathVariable int id) {
+        for (Quiz quiz : quizzes) {
+            if (quiz.getId() == id) return new ResponseEntity<>(quiz, HttpStatus.OK);
         }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping(path = "/api/quizzes/{id}/solve")
+    public ResponseEntity<SolveQuizResponse> solveQuiz(@PathVariable int id, @RequestParam("answer") int answer) {
+        for (Quiz quiz : quizzes) {
+            if (quiz.getId() == id) {
+                if (quiz.getAnswer() == answer) {
+                    return new ResponseEntity<>(new SolveQuizResponse(true, "Congratulations, you're right!"), HttpStatus.OK);
+                } else {
+                    return new ResponseEntity<>(new SolveQuizResponse(false, "Wrong answer! Please, try again."), HttpStatus.OK);
+                }
+            }
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }
