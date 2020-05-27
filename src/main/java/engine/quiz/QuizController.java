5b65724eafccfe0f9e7f740b9771626767a01e15
Igor <GI_Something@protonmail.com>
2020-05-27 10:57:21 +0300

    refactor: Move code from QuizController to QuizService

--- src/main/java/engine/quiz/QuizController.java
+++ src/main/java/engine/quiz/QuizController.java
@@ -4,85 +4,45 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
 import java.security.Principal;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 @RestController
 public class QuizController {
     @Autowired
-    QuizRepository quizRepo;
+    private QuizService quizService;
 
     @PostMapping(path = "/api/quizzes")
     public Quiz addQuiz(@Valid @RequestBody Quiz quiz, @AuthenticationPrincipal Principal principal) {
-        throwIfAnswerIsInvalid(quiz.getOptions(), quiz.getAnswer());
-        if (!onlyUniqueValues(quiz.getOptions()))
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Duplicate detected in options");
-        quiz.setCreatedBy(principal.getName());
-        return quizRepo.save(quiz);
+        return quizService.addQuiz(quiz, principal);
     }
 
     @GetMapping(path = "/api/quizzes/{id}")
     public Quiz getQuiz(@PathVariable long id) {
-        return quizRepo.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        return quizService.getQuiz(id);
     }
 
     @GetMapping(path = "/api/quizzes")
     public Iterable<Quiz> getAllQuizzes() {
-        return quizRepo.findAll();
+        return quizService.getAllQuizzes();
     }
 
     @PostMapping(path = "/api/quizzes/{id}/solve")
     public SolveQuizResponse solveQuiz(@PathVariable long id, @RequestBody Map<String, List<Integer>> mapAnswer) {
-        Quiz quiz = quizRepo.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
-
-        List<Integer> answer = mapAnswer.get("answer");
-        if (answer == null || mapAnswer.size() != 1) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Incorrect data");
-        }
-        throwIfAnswerIsInvalid(quiz.getOptions(), answer);
-
-        if (equalsIgnoreOrder(quiz.getAnswer(), answer)) {
-            return new SolveQuizResponse(true, "Congratulations, you're right!");
-        } else {
-            return new SolveQuizResponse(false, "Wrong answer! Please, try again.");
-        }
+        return quizService.solveQuiz(id, mapAnswer);
     }
 
     @ResponseStatus(code = HttpStatus.NO_CONTENT)
     @DeleteMapping(path = "/api/quizzes/{id}")
     public void deleteQuiz(@PathVariable long id, @AuthenticationPrincipal Principal principal) {
-        Quiz quiz = quizRepo.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
-        if (!quiz.getCreatedBy().equals(principal.getName())) throw new ResponseStatusException(HttpStatus.FORBIDDEN);
-        quizRepo.deleteById(id);
+        quizService.deleteQuiz(id, principal);
     }
 
     @PutMapping(path = "/api/quizzes/{id}")
     public Quiz updateQuiz(@PathVariable long id, @Valid @RequestBody Quiz updatedQuiz, @AuthenticationPrincipal Principal principal) {
-        Quiz quiz = quizRepo.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
-        if (!quiz.getCreatedBy().equals(principal.getName())) throw new ResponseStatusException(HttpStatus.FORBIDDEN);
-        updatedQuiz.setId(id);
-        return addQuiz(updatedQuiz, principal);
-    }
-
-    private static void throwIfAnswerIsInvalid(List<String> options, List<Integer> answer) {
-        if (answer.stream().distinct().count() != answer.size()
-                || answer.size() > options.size()
-                || (answer.size() > 0 &&
-                (answer.stream().max(Integer::compare).orElseThrow() >= options.size() || answer.stream().min(Integer::compare).orElseThrow() < 0))) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "answer is not valid");
-        }
-    }
-
-    private static <T> boolean onlyUniqueValues(List<T> data) {
-        return data.stream().distinct().count() == data.size();
-    }
-
-    private static <T> boolean equalsIgnoreOrder(List<T> one, List<T> two) {
-        return one.stream().sorted().collect(Collectors.toList()).equals(two.stream().sorted().collect(Collectors.toList()));
+        return quizService.updateQuiz(id, updatedQuiz, principal);
     }
 }
---
+++ src/main/java/engine/quiz/QuizService.java
@@ -0,0 +1,79 @@
+package engine.quiz;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.security.Principal;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Service
+public class QuizService {
+    @Autowired
+    private QuizRepository quizRepo;
+
+    public Quiz addQuiz(Quiz quiz, Principal principal) {
+        throwIfAnswerIsInvalid(quiz.getOptions(), quiz.getAnswer());
+        if (!onlyUniqueValues(quiz.getOptions()))
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Duplicate detected in options");
+        quiz.setCreatedBy(principal.getName());
+        return quizRepo.save(quiz);
+    }
+
+    public Quiz getQuiz(long id) {
+        return quizRepo.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    public Iterable<Quiz> getAllQuizzes() {
+        return quizRepo.findAll();
+    }
+
+    public SolveQuizResponse solveQuiz(long id, Map<String, List<Integer>> mapAnswer) {
+        Quiz quiz = quizRepo.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        List<Integer> answer = mapAnswer.get("answer");
+        if (answer == null || mapAnswer.size() != 1) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Incorrect data");
+        }
+        throwIfAnswerIsInvalid(quiz.getOptions(), answer);
+
+        if (equalsIgnoreOrder(quiz.getAnswer(), answer)) {
+            return new SolveQuizResponse(true, "Congratulations, you're right!");
+        } else {
+            return new SolveQuizResponse(false, "Wrong answer! Please, try again.");
+        }
+    }
+
+    public void deleteQuiz(long id, Principal principal) {
+        Quiz quiz = quizRepo.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        if (!quiz.getCreatedBy().equals(principal.getName())) throw new ResponseStatusException(HttpStatus.FORBIDDEN);
+        quizRepo.deleteById(id);
+    }
+
+    public Quiz updateQuiz(long id, Quiz updatedQuiz, Principal principal) {
+        Quiz quiz = quizRepo.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        if (!quiz.getCreatedBy().equals(principal.getName())) throw new ResponseStatusException(HttpStatus.FORBIDDEN);
+        updatedQuiz.setId(id);
+        return addQuiz(updatedQuiz, principal);
+    }
+
+    private static void throwIfAnswerIsInvalid(List<String> options, List<Integer> answer) {
+        if (answer.stream().distinct().count() != answer.size()
+                || answer.size() > options.size()
+                || (answer.size() > 0 &&
+                (answer.stream().max(Integer::compare).orElseThrow() >= options.size() || answer.stream().min(Integer::compare).orElseThrow() < 0))) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "answer is not valid");
+        }
+    }
+
+    private static <T> boolean onlyUniqueValues(List<T> data) {
+        return data.stream().distinct().count() == data.size();
+    }
+
+    private static <T> boolean equalsIgnoreOrder(List<T> one, List<T> two) {
+        return one.stream().sorted().collect(Collectors.toList()).equals(two.stream().sorted().collect(Collectors.toList()));
+    }
+}
